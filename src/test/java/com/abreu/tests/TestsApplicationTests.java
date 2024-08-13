8ef1d11aa3a81a67c05e43de857091de9c1edf0d
Igor Nascimento <igor.nacimento51@gmail.com>
2024-08-13 13:05:33 -0300

    feat: add initial structure

---
+++ src/test/java/com/abreu/tests/TestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abreu.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
