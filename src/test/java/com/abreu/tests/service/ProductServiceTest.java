4a28df8a8d49de698f12ff07134474b7489d572c
Igor Nascimento <igor.nacimento51@gmail.com>
2024-08-13 13:32:38 -0300

    feat: add findAll test

--- src/test/java/com/abreu/tests/service/ProductServiceTest.java
+++ src/test/java/com/abreu/tests/service/ProductServiceTest.java
@@ -11,10 +11,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
@@ -23,6 +24,8 @@ public class ProductServiceTest {
     public static final String DESCRIPTION = "description";
     public static final String NAME = "name";
     public static final long ID = 1L;
+    public static final int INDEX = 0;
+
     @Mock
     private ProductRepository productRepository;
 
@@ -71,6 +74,32 @@ public class ProductServiceTest {
 
             assertEquals("Product not found!", exception.getMessage());
         }
+    }
+
+    @Nested
+    class FindAll {
+
+        @Test
+        @DisplayName("Should return with success all products")
+        void shouldReturnWithSuccessAllProducts() {
+
+            when(productRepository.findAll()).thenReturn(List.of(product));
+
+            var response = productService.findAll();
+
+            assertNotNull(response);
+
+            assertEquals(1, response.size());
+            assertEquals(Product.class, response.get(INDEX).getClass());
+
+            assertEquals(ID, response.get(INDEX).getId());
+            assertEquals(NAME, response.get(INDEX).getName());
+            assertEquals(DESCRIPTION, response.get(INDEX).getDescription());
+            assertEquals(EMAIL, response.get(INDEX).getEmail());
+
+            verify(productRepository, times(1)).findAll();
+        }
+
     }
 
     private void startProduct() {
