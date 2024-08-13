3467c6438ba770b247d8bf5740af7a973f78b451
Igor Nascimento <igor.nacimento51@gmail.com>
2024-08-13 13:10:13 -0300

    feat: creating product model

---
+++ src/main/java/com/abreu/tests/model/Product.java
@@ -0,0 +1,25 @@
+package com.abreu.tests.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_products")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private String email;
+
+}
