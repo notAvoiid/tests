4b947d5c5967b63ea5ebfddcc12b9d2517730311
Igor Nascimento <igor.nacimento51@gmail.com>
2024-08-14 14:27:27 -0300

    feat: add product controller test find all

---
+++ src/test/java/com/abreu/tests/controller/ProductControllerTest.java
@@ -0,0 +1,80 @@
+package com.abreu.tests.controller;
+
+import com.abreu.tests.model.Product;
+import com.abreu.tests.model.dto.ProductDTO;
+import com.abreu.tests.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductControllerTest {
+
+    private static final long ID = 1L;
+    private static final String NAME = "name";
+    private static final String DESCRIPTION = "description";
+    private static final String EMAIL = "fulanodetal@gmail.com";
+    public static final int INDEX = 0;
+
+    @InjectMocks
+    private ProductController productController;
+
+    @Mock
+    private ProductService productService;
+
+    static Product product;
+    static ProductDTO productDTO;
+    static Optional<Product> optionalProduct;
+
+    @BeforeEach
+    void setUp() {
+        startProduct();
+    }
+
+    @Nested
+    class FindAll {
+
+        @Test
+        @DisplayName("Should return all products when success")
+        void shouldReturnAllProductsWhenSuccess(){
+            when(productService.findAll()).thenReturn(List.of(product));
+
+            var response = productController.findAll();
+
+            assertNotNull(response);
+            assertNotNull(response.getBody());
+
+            assertEquals(HttpStatus.OK, response.getStatusCode());
+
+            assertEquals(ResponseEntity.class, response.getClass());
+            assertInstanceOf(List.class, response.getBody());
+            assertEquals(Product.class, response.getBody().get(INDEX).getClass());
+            assertEquals(ID, response.getBody().get(INDEX).getId());
+            assertEquals(NAME, response.getBody().get(INDEX).getName());
+            assertEquals(DESCRIPTION, response.getBody().get(INDEX).getDescription());
+            assertEquals(EMAIL, response.getBody().get(INDEX).getEmail());
+
+            verify(productService, times(1)).findAll();
+        }
+    }
+
+    private void startProduct() {
+        product = new Product(ID, NAME, DESCRIPTION, EMAIL);
+        productDTO = new ProductDTO(ID, NAME, DESCRIPTION, EMAIL);
+        optionalProduct = Optional.of(product);
+    }
+
+}
