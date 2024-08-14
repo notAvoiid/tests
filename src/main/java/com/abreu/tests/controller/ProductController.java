a6791a5d0b16fbf0a97703a543ca932376002418
Igor Nascimento <igor.nacimento51@gmail.com>
2024-08-14 14:27:05 -0300

    feat: add product controller and find all

---
+++ src/main/java/com/abreu/tests/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.abreu.tests.controller;
+
+import com.abreu.tests.model.Product;
+import com.abreu.tests.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll() {
+        return ResponseEntity.ok(productService.findAll());
+    }
+}
